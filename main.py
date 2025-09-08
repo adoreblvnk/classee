9b190e23c5259c4bed383b735cdecf0254086451
adoreblvnk <83258252+adoreblvnk@users.noreply.github.com>
2025-09-08 14:34:30 +0800

    feat: instantiate fastapi

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from stock-market-analysis!")
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
