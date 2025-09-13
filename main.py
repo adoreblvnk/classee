b147f64b406c4613cd7f8c868d34ba9489e718b2
adoreblvnk <83258252+adoreblvnk@users.noreply.github.com>
2025-09-13 11:24:49 +0800

    refactor: move to algo folder

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, HTTPException
 import uvicorn
 import pandas as pd
 from pathlib import Path
-from max_prof.max_prof import MaxProf
-from sma_return.sma_return import SMA_Return
-from up_down_runs.up_down_runs import analyze_up_down_runs
+from algo.max_prof import MaxProf
+from algo.sma_return import SMA_Return
+from algo.up_down_runs import analyze_up_down_runs
 
 # load dataset
 DATASET = pd.read_csv(Path(__file__).parent / "data" / "TSLA.csv")
